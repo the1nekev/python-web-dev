5dcce27934ac5f485599727e61824585fbbee19f
the1nekev <kevcorcino15@gmail.com>
2024-01-14 09:54:02 -0500

    Exercise 2.4

--- achievement-2/Exercise 2.3/Recipe-App/src/recipeapp/urls.py
+++ achievement-2/Exercise 2.3/Recipe-App/src/recipeapp/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for bookstore project.
+URL configuration for recipeapp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sales.urls'))
+    path('', include('recipes.urls')),
 ]
